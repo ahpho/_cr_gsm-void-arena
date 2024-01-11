e3006b5d13c43ed7459e90211789507325f6733d
邝思豪 <kuangsihao@sandboxol.com>
2024-01-11 22:11:35 +0800

    换StreamWriter构造方法，成功Flush到文件。

--- Program.cs
+++ Program.cs
@@ -8,8 +8,7 @@ using System.Collections.Generic;
 List<DeviceInfo> inputs = TabHandler.Read("Input.tab");
 List<DeviceInfo> outputs = new List<DeviceInfo>();
 
-FileStream fs = File.Create("Output.tab");
-StreamWriter sw = new StreamWriter(fs);
+StreamWriter sw = new StreamWriter("Output.tab");
 sw.WriteLine("#name\t#count\t#memory\t#chipset\t#cpu_count\t#cpu_freq\t#cpu\t#gpu\t#launch\t#price\t#os");
 
 int TestCount = 0;
@@ -26,7 +25,6 @@ foreach (DeviceInfo input in inputs)
     string line = string.Format($"{info.name}\t{info.count}\t{info.memory}\t{info.chipset}\t{info.cpu_count}\t{info.cpu_freq}\t{info.cpu}\t{info.gpu}\t{info.launch}\t{info.price}\t{info.android}");
     sw.WriteLine(line);
     sw.Flush();
-    fs.Flush();
 
     Console.WriteLine(info.name);
     Thread.Sleep(2000);
@@ -34,6 +32,6 @@ foreach (DeviceInfo input in inputs)
     if (++TestCount > 10 && false)
         break;
 }
+
 sw.Close();
-fs.Close();
 Console.WriteLine("All Done!");
