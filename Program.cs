2eace46970083c0d1ba0e2c3756f6651e0520041
邝思豪 <kuangsihao@sandboxol.com>
2024-01-12 18:05:12 +0800

    支持只重新处理带error的部分

--- Program.cs
+++ Program.cs
@@ -5,39 +5,44 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
-List<DeviceInfo> inputs = TabHandler.Read("Input.tab");
-List<DeviceInfo> outputs = new List<DeviceInfo>();
-
+string[] lines = File.ReadAllLines("Input.tab", Encoding.UTF8);
 StreamWriter sw = new StreamWriter("Output.tab");
-sw.WriteLine("#name\t#count\t#memory\t#chipset\t#cpu_count\t#cpu_freq\t#cpu\t#gpu\t#launch\t#price\t#os");
+//sw.WriteLine("#name\t#count\t#memory\t#chipset\t#cpu_count\t#cpu_freq\t#cpu\t#gpu\t#launch\t#price\t#os");
 
 int TestCount = 0;
-foreach (DeviceInfo input in inputs)
+foreach (string line in lines)
 {
-    DeviceInfo d = new DeviceInfo(input.name, input.count);
-
+    string outLine = line;
+    string name = "error", count = "error";
     try
     {
-        string url = Crawler.GetUrlFromGoogle(input.name);
-        string page = Crawler.GetPageContent(url);
-        if (string.IsNullOrEmpty(page)) throw new Exception("page is empty !");
-        d = Gsmarena.Parse(page);
+        if (!line.StartsWith('#'))
+        {
+            string[] columns = line.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            if (columns.Length == 2 || (columns.Length > 3 && columns[2] == "error"))
+            {
+                name = columns[0];
+                count = columns[1];
+                DeviceInfo d = new DeviceInfo(name, count);
+                string url = Crawler.GetUrlFromGoogle(name);
+                string page = Crawler.GetPageContent(url);
+                if (string.IsNullOrEmpty(page)) throw new Exception("page is empty !");
+                d = Gsmarena.Parse(page);
+                d.name = name;
+                d.count = count;
+                outLine = string.Format($"{d.name}\t{d.count}\t{d.memory}\t{d.chipset}\t{d.cpu_count}\t{d.cpu_freq}\t{d.cpu}\t{d.gpu}\t{d.launch}\t{d.price}\t{d.android}");
+                Console.WriteLine(name);
+                Thread.Sleep(2000);
+            }
+        }
     }
     catch (Exception ex)
     {
-        Console.WriteLine(string.Format($"[for] Exception, device.name={input.name}, ex={ex}"));
+        Console.WriteLine(string.Format($"[for] Exception, device.name={name}, ex={ex}"));
     }
 
-    d.name = input.name;
-    d.count = input.count;
-    outputs.Add(d);
-
-    string line = string.Format($"{d.name}\t{d.count}\t{d.memory}\t{d.chipset}\t{d.cpu_count}\t{d.cpu_freq}\t{d.cpu}\t{d.gpu}\t{d.launch}\t{d.price}\t{d.android}");
-    sw.WriteLine(line);
+    sw.WriteLine(outLine);
     sw.Flush();
-
-    Console.WriteLine(d.name);
-    Thread.Sleep(2000);
 
     if (++TestCount > 10 && false)
         break;
