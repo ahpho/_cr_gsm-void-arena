d96a660313cd082fd829830fe20d79dc501f58a9
邝思豪 <kuangsihao@sandboxol.com>
2024-01-12 18:06:36 +0800

    增加Sqlite（暂时没用）

---
+++ Sqlite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+public class Sqlite // 暂时没用
+{
+    public static void Do(string inFile)
+    {
+        SQLiteConnection connection = new SQLiteConnection(GetConn());
+    }
+
+    private static string GetConn()
+    {
+        return @"data source="".\sqlite.db; version = 3;";
+    }
+}
